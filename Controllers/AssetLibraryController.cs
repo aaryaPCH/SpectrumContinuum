c95feeabaefbbc988bfadf7cfc1176376be3d8bc
Ash Arya <aarya@pch.com>
2013-12-04 09:48:04 -0500

    Initial commit
    Basic MVC 4 + bootstrap and jquery added

---
+++ Controllers/AssetLibraryController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SpectrumContinuum.Controllers
+{
+    public class AssetLibraryController : Controller
+    {
+        //
+        // GET: /AssetLibrary/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
