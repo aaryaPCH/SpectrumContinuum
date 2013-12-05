c8e9b97c10c3abb4a7854a34099b5fc6bccb2a5a
Ash Arya <aarya@pch.com>
2013-12-04 22:40:18 -0500

    Added icons, package nodes and assetService.js
    - Moved code around to make it modular
    - Expanded the asset library template to include icons and package nodes
    - Added assetService.js to simulate AJAX calls

---
+++ Scripts/App/assetLibrary.js
@@ -0,0 +1,81 @@
+﻿var continuum = continuum || angular.module("Continuum", []);
+continuum.controller("AssetLibController", function ($scope) {
+    // Init
+    $scope.item = {
+        expanded: true,
+        children: [],
+    };
+    $scope.assetService = new continuum.AssetService();
+    $scope.assetService.getRootFolders(function (rootFolders) {
+        $scope.item.children = rootFolders;
+        $scope.$apply();
+    });
+
+    // On item click
+    $scope.onItemClick = function ($event) {
+        $event.stopPropagation();
+
+        if ($scope.isFolder(this.item)) {
+            handleFolderClick(this.item);
+        }
+        else if ($scope.isPackage(this.item)) {
+            console.log("package clicked", this.item);
+        }
+    };
+
+    $scope.isFolder = function (item) {
+        return item.type == "folder";
+    };
+
+    $scope.isPackage = function (item) {
+        return item.type == "package";
+    };
+
+    $scope.isOpenFolder = function (item) {
+        return $scope.isFolder(item) && item.expanded;
+    };
+
+    $scope.isClosedFolder = function (item) {
+        return $scope.isFolder(item) && !item.expanded;
+    };
+
+
+    // -- Private / Helper functions --
+    function handleFolderClick(folder) {
+        // Toggle folder state
+        folder.expanded = !folder.expanded;
+        if (folder.expanded) {
+            // Collapse the expanded folders except the one just being expanded
+            collapseAllExcept(folder);
+
+            // Populate current folder
+            $scope.assetService.getChildren(folder, function (children) {
+                folder.children = children;
+                $scope.$apply();
+            });
+        }
+    }
+
+    function collapseAllExcept(givenFolder) {
+        var rootFolders = $scope.$root.$$childHead.item.children;
+        for (var index in rootFolders) {
+            // don't collapse if it is current folder or an ancestor
+            var rootFolder = rootFolders[index];
+            if (rootFolder == givenFolder || isAncestor(rootFolder, givenFolder)) continue;
+
+            // all other folders may be collapsed at the root level
+            rootFolder.expanded = false;
+        }
+    }
+
+    function isAncestor(ancestor, descendant) {
+        if (!ancestor || !ancestor.children || !ancestor.children.length) return false;
+        for (var index in ancestor.children) {
+            var child = ancestor.children[index];
+            if (child == descendant || isAncestor(child, descendant)) return true;
+        }
+
+        // no match found
+        return false;
+    }
+});
---
+++ Scripts/App/assetService.js
@@ -0,0 +1,29 @@
+﻿var continuum = continuum || angular.module("Continuum", []);
+continuum.AssetService = (function ($module) {
+    var waitTime = 2000;
+    function assetService() {
+    };
+
+    assetService.prototype.getRootFolders = function (callback) {
+        this.getChildren(null, callback);
+    };
+
+    assetService.prototype.getChildren = function (folder, callback) {
+        var children = [{ name: "Cras justo odio", type: "folder" },
+            { name: "Dapibus ac facilisis in", type: "folder" },
+            { name: "Morbi leo risus", type: "folder" },
+            { name: "Porta ac consectetur ac", type: "folder" }];
+        if (folder != null) {
+            children.push({ name: "Vestibulum at eros", type: "package" });
+        }
+        delay(callback, children);
+    };
+
+    var delay = function (callback, params) {
+        window.setTimeout(function () {
+            callback.call(this, params);
+        }, waitTime);
+    };
+
+    return assetService;
+}(continuum));
